0d1a3acf5f7c7382d1a4c22c094c4c6988b22a25
Zhihao Cui <zcui@caltech.edu>
2018-11-06 01:57:18 -0800

    add files

---
+++ test_python.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+import numpy as np
+import numpy.linalg as la
+
+rho1 = np.load('./rho1.npy')
+u1, s1, vt1 = la.svd(rho1, full_matrices = False)
+rho2 = np.load('./rho2.npy')
+u2, s2, vt2 = la.svd(rho2, full_matrices = False)
+
+print la.norm(rho1-rho2)
+print la.norm(s1-s2)
+print la.norm(vt1-vt2)
+
+
